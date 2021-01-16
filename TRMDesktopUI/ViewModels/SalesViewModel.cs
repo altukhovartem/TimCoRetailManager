102fb40c9e1796774512c03049c184b0621e7e13
aaltukhov <aaltukhov@acumatica.com>
2021-01-16 20:56:30 +0300

    get Add to cart working

--- TRMDesktopUI/ViewModels/SalesViewModel.cs
+++ TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -43,9 +43,23 @@ namespace TRMDesktopUI.ViewModels
 			}
 		}
 
-		private BindingList<ProductModel> _cart;
+		private ProductModel _selectedProduct;
 
-		public BindingList<ProductModel> Cart
+		public ProductModel SelectedProduct
+		{
+			get { return _selectedProduct; }
+			set 
+			{ 
+				_selectedProduct = value;
+				NotifyOfPropertyChange(() => SelectedProduct);
+				NotifyOfPropertyChange(() => CanAddToCart);
+			}
+		}
+
+
+		private BindingList<CartItemModel> _cart = new BindingList<CartItemModel>();
+
+		public BindingList<CartItemModel> Cart
 		{
 			get { return _cart; }
 			set
@@ -55,7 +69,7 @@ namespace TRMDesktopUI.ViewModels
 			}
 		}
 
-		private int _itemQuantity;
+		private int _itemQuantity = 1;
 
 		public int ItemQuantity
 		{
@@ -64,6 +78,7 @@ namespace TRMDesktopUI.ViewModels
 			{ 
 				_itemQuantity = value;
 				NotifyOfPropertyChange(() => ItemQuantity);
+				NotifyOfPropertyChange(() => CanAddToCart);
 			}
 		}
 
@@ -71,8 +86,12 @@ namespace TRMDesktopUI.ViewModels
 		{ 
 			get
 			{
-				// TODO - replace with calculation
-				return "$0.00";
+				decimal subTotal = 0;
+				foreach (var item in Cart)
+				{
+					subTotal+= item.Product.RetailPrice * item.QuantityInCart;
+				}
+				return subTotal.ToString("C");
 			}
 		}
 
@@ -103,13 +122,39 @@ namespace TRMDesktopUI.ViewModels
 				// make sure someting is selected
 				// make sure there is an item quantity
 
+				if(ItemQuantity > 0 && SelectedProduct?.QuantityInStock >= ItemQuantity )
+				{
+					output = true;
+				}
+
 				return output;
 			}
 		}
 
 		public void AddToCart()
 		{
+			CartItemModel existingItem = Cart.FirstOrDefault(x => x.Product == SelectedProduct);
+			if(existingItem != null)
+			{
+				existingItem.QuantityInCart += ItemQuantity;
+				Cart.Remove(existingItem);
+				Cart.Add(existingItem);
+			}
+			else
+			{
+				CartItemModel item = new CartItemModel
+				{
+					Product = SelectedProduct,
+					QuantityInCart = ItemQuantity
+				};
 
+				Cart.Add(item);
+			}
+
+			
+			SelectedProduct.QuantityInStock -= ItemQuantity;
+			ItemQuantity = 1;
+			NotifyOfPropertyChange(() => SubTotal);
 		}
 
 		public bool CanRemoveFromCart
@@ -126,7 +171,7 @@ namespace TRMDesktopUI.ViewModels
 
 		public void RemoveFromCart()
 		{
-
+			NotifyOfPropertyChange(() => SubTotal);
 		}
 
 		public bool CanCheckOut
