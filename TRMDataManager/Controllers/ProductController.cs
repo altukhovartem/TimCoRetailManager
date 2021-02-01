f633c819dc398231725e6fc1fab13b590ebf009c
aaltukhov <aaltukhov@acumatica.com>
2021-02-01 08:20:40 +0300

    adds user permissions and error handling for ProductController

--- TRMDataManager/Controllers/ProductController.cs
+++ TRMDataManager/Controllers/ProductController.cs
@@ -9,7 +9,7 @@ using TRMDataManager.Library.Models;
 
 namespace TRMDataManager.Controllers
 {
-    [Authorize(Roles = "Cashies") ]
+    [Authorize(Roles = "Cashier") ]
     public class ProductController : ApiController
     {
         public List<ProductModel> Get()
