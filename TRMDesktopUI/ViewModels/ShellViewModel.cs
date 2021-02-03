f8aeb7fd193fcdb3d64c226d18f791cc3f4baee2
aaltukhov <aaltukhov@acumatica.com>
2021-02-03 06:33:41 +0300

    adds the User / Role form

--- TRMDesktopUI/ViewModels/ShellViewModel.cs
+++ TRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -48,6 +48,11 @@ namespace TRMDesktopUI.ViewModels
 			TryClose();
 		}
 
+		public void UserManagment()
+		{
+			ActivateItem(IoC.Get<UserDisplayViewModel>());
+		}
+
 		public void LogOut()
 		{
 			_user.ResetUserModel();
